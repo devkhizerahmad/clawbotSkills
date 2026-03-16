e4b96a9dd09c2ce453061f8ae994f3bae30992d6
Khizar Ahmed <devkhizerahmad@gmail.com>
2026-03-16 15:14:22 +0500

    updated

--- google-sheets-api/test-format-date.js
+++ google-sheets-api/test-format-date.js
@@ -46,4 +46,4 @@ if (require.main === module) {
 module.exports = { testFormatCleaningDateCell };
 
 
- adsa node scripts/sheets-cli.js write "1RobrLNYSmMUyq53dUcdmj2ePaU2YkagqLqgIgx7M4OU" "Cleaning!X23" "@test-data.json"
+ a node scripts/sheets-cli.js write "1RobrLNYSmMUyq53dUcdmj2ePaU2YkagqLqgIgx7M4OU" "Cleaning!X23" "@test-data.json"
